a3527f16ee3105fd070b20c9ca38027911f10005
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-17 19:04:09 -0800

    numeric

--- bindings.js
+++ bindings.js
@@ -38,6 +38,36 @@ ko.bindingHandlers.visibleWithClass = {
     });
   }
 };
+
+ko.extenders.numeric = function(target, precision) {
+    //create a writable computed observable to intercept writes to our observable
+    var result = ko.pureComputed({
+        read: target,  //always return the original observables value
+        write: function(newValue) {
+            var current = target(),
+                roundingMultiplier = Math.pow(10, precision),
+                newValueAsNum = isNaN(newValue) ? 0 : parseFloat(+newValue),
+                valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
+ 
+            //only write if it changed
+            if (valueToWrite !== current) {
+                target(valueToWrite);
+            } else {
+                //if the rounded value is the same, but a different value was written, force a notification for the current field
+                if (newValue !== current) {
+                    target.notifySubscribers(valueToWrite);
+                }
+            }
+        }
+    }).extend({ notify: 'always' });
+ 
+    //initialize with current value to make sure it is rounded appropriately
+    result(target());
+ 
+    //return the new computed observable
+    return result;
+};
+
 //Takes {observ, key}.
 //Persists the observ, and shares the backing storage with everything that has the same key,
 //  so if an observable with the same key changes, our obserable also changes (and the persisted value changes).
